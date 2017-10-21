39ce3e8c20b45acb8ea4ca37287cb17428257809
Terri Hall <webdev.terrihall@gmail.com>
2017-10-21 19:10:03 -0400

    Add files via upload

---
+++ Thall-RangeFunctionOneParam.py
@@ -0,0 +1,4 @@
+my_list = [0,1,2,3]
+my_list_len = len(my_list)
+for i in range(0, my_list_len):
+    print(my_list[i])
---
+++ Thall-RangeFunctionThreeParam2.py
@@ -0,0 +1,4 @@
+my_list = [8, 6, 4, 2]
+my_list_len = len(my_list)
+for i in range(8, 2, -2):
+    print(my_list)
